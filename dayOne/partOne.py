15dc5273b2b3cfff929a93efb4d5a9f3dfd49ffb
Roshni Vachhani <Roshni.Vachhani@gmail.com>
2020-12-01 11:41:40 +0000

    Update partOne.py

--- dayOne/partOne.py
+++ dayOne/partOne.py
@@ -1,1 +1,9 @@
-
+data = set()                                                  
+with open('input.txt', 'r') as f:                                               
+    for line in f:                                            
+        i = int(line)                                                           
+        if 2020 - i in data:                                                 
+            print(i*(2020 - i))                                                 
+            break                                                               
+        else:                                                                   
+            data |= {i}
