e9f5de9c5118aee67559a0ee5041ac678f76003d
Roshni Vachhani <Roshni.Vachhani@gmail.com>
2020-12-01 11:48:03 +0000

    Update partTwo.py

--- dayOne/partTwo.py
+++ dayOne/partTwo.py
@@ -1,1 +1,8 @@
-
+import sys                                                                      
+with open('input.txt', 'r') as f:                                               
+    dat = {int(line) for line in f}                                         
+for i in dat:                                                               
+    for j in dat:                                                           
+        if 2020 - i - j in dat:                                             
+            print(i * (2020 - i - j) * j)                                       
+            sys.exit()  
