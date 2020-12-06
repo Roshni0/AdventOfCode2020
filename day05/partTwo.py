95e25b49bb1323722c01e1a88372cf1e4b5e8a14
Roshni Vachhani <Roshni.Vachhani@gmail.com>
2020-12-06 11:47:25 +0000

    Create partTwo.py

---
+++ day05/partTwo.py
@@ -0,0 +1,7 @@
+def get_seat_id(s):
+  return int(s.replace('F','0').replace('B','1')
+              .replace('L','0').replace('R','1'), 2)
+seat_ids = sorted(get_seat_id(s) for s in open('input.txt').readlines())
+for i, v in enumerate(seat_ids):
+  if i > 0 and seat_ids[i-1]+1 != v:
+    print("Missing:", v-1)
