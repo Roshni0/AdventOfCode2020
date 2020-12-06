60c56e818699a2c51f2fdc9defba294c7a8b2dcb
Roshni Vachhani <Roshni.Vachhani@gmail.com>
2020-12-06 11:53:29 +0000

    Create partTwo.py

---
+++ day06/partTwo.py
@@ -0,0 +1,12 @@
+input = open("input.txt", "r")
+total = 0
+data=''
+for line in input:
+    if len(line)>1:
+        data += line.strip()+' '
+    else: data+=line
+data = data.split('\n')
+for record in data:
+    record = sorted(record.split(), key=len)
+    total+= sum(1 for i in record[0] if all(i in j for j in record))
+print(total)
